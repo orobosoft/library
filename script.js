f6e6fe1dd1a1a25d61c57953b336beaa9d8a7865
Orobosa <95650588+orobosaik@users.noreply.github.com>
2022-07-04 21:53:04 +0100

    Refactor constructor to class

--- script.js
+++ script.js
@@ -2,24 +2,28 @@
 let myLibrary = [];
 
 //BOOK CONSTRUCTOR
-function Book(title, author, numOfPages, readStatus) {
-	this.title = title;
-	this.author = author;
-	this.numOfPages = numOfPages;
-	this.readStatus = readStatus;
-}
-Book.prototype.info = function () {
-	console.log(
-		`${this.title} by ${this.author}, ${this.numOfPages} pages, ${this.readStatus}`
-	);
-};
-Book.prototype.toggleRead = function () {
-	let stats = this.readStatus;
+class Book {
+	constructor(title, author, numOfPages, readStatus) {
+		this.title = title;
+		this.author = author;
+		this.numOfPages = numOfPages;
+		this.readStatus = readStatus;
+	}
 
-	stats === "read already"
-		? (this.readStatus = "not read yet")
-		: (this.readStatus = "read already");
-};
+	info() {
+		console.log(
+			`${this.title} by ${this.author}, ${this.numOfPages} pages, ${this.readStatus}`
+		);
+	}
+
+	toggleRead() {
+		let stats = this.readStatus;
+
+		stats === "read already"
+			? (this.readStatus = "not read yet")
+			: (this.readStatus = "read already");
+	}
+}
 
 //Add book to myLibrary Array and display the books
 function addBookToLibrary(formData) {
